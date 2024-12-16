1e1186e0275b4248c1a858f9bf302c7660c400e7
Isabel <isabel.dapper@agrocpr.com>
2024-12-16 19:38:17 -0300

    Add data json and update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import './App.css'
+import './global.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Header from './components/header'
+
+import { ActionModalProvider } from './context/action-modal';
+import { ToastProvider } from './context/toast-context';
+
+import Header from './components/header';
 import Footer from './components/footer';
-import TabelaAvisos from './components/notice-board';
+import MonthCalendar from './components/calendar';
+import NoticeTable from './components/notice-table';
+import QuickAccess from './components/quick-access';
+import ToastNotification from './components/toast-notification';
+
 
 function App() {
-
   return (
-    <div className='d-flex flex-column min-vh-100 justify-content-between'>
-      <Header />
-      <TabelaAvisos />
-      <Footer />
-    </div>
-  )
+    <ActionModalProvider>
+      <ToastProvider> 
+        <div className='d-flex flex-column min-vh-100'>
+          <Header />
+          <div className='container-fluid flex-grow-1 mx-2'>
+            <div className='row'>
+
+              <div className='col-8'>
+                <QuickAccess />
+                <NoticeTable />
+              </div>
+
+              <div className='col-4'>
+                <MonthCalendar />
+              </div>
+            </div>
+          </div>
+
+          <ToastNotification /> 
+          <Footer />
+        </div>
+      </ToastProvider>
+    </ActionModalProvider>
+  );
 }
 
-export default App
+export default App;
